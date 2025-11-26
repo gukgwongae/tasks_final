6b398746fafff8ec5d297b162c0242163bd0e71d
GukGwonGae <gukgwongae@gmail.com>
2025-11-26 15:52:37 +0900

    feature: new proj

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tasks_final
+<<<<<<< HEAD
 스파르타 심화 개인과제 -To Do App 고도화 하기
+=======
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+>>>>>>> 7271ec3 (feature: new proj)
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tasks/core/app_theme.dart';
+import 'package:tasks/core/firebase_options.dart';
+import 'package:tasks/ui/home_page/home_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
+      home: const HomePage(title: '스파르타`s Tasks'),
+    );
+  }
+}
